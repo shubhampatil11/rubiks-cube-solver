3fa99d2e9b260fa290c0beb394730ce714cdcb11
Shubham Patil <shubhampatil11698@gmail.com>
2021-12-17 15:34:52 +0530

    Initial implementation

---
+++ Model/RubiksCube.cpp
@@ -0,0 +1,299 @@
+//
+// Created by Shubham Patil on 17/12/21.
+//
+
+#include "RubiksCube.h"
+
+/*
+ * Describe a move using an index
+ */
+string RubiksCube::getMove(MOVE ind) const {
+    switch (ind) {
+        case MOVE::L:
+            return "L";
+        case MOVE::LPRIME:
+            return "L'";
+        case MOVE::L2:
+            return "L2";
+        case MOVE::R:
+            return "R";
+        case MOVE::RPRIME:
+            return "R'";
+        case MOVE::R2:
+            return "R2";
+        case MOVE::U:
+            return "U";
+        case MOVE::UPRIME:
+            return "U'";
+        case MOVE::U2:
+            return "U2";
+        case MOVE::D:
+            return "D";
+        case MOVE::DPRIME:
+            return "D'";
+        case MOVE::D2:
+            return "D2";
+        case MOVE::F:
+            return "F";
+        case MOVE::FPRIME:
+            return "F'";
+        case MOVE::F2:
+            return "F2";
+        case MOVE::B:
+            return "B";
+        case MOVE::BPRIME:
+            return "B'";
+        case MOVE::B2:
+            return "B2";
+    }
+}
+
+/*
+ * Perform a move operation on using a Move index.
+ */
+RubiksCube &RubiksCube::move(MOVE ind) {
+    switch (ind) {
+        case MOVE::L:
+            return this->l();
+        case MOVE::LPRIME:
+            return this->lPrime();
+        case MOVE::L2:
+            return this->l2();
+        case MOVE::R:
+            return this->r();
+        case MOVE::RPRIME:
+            return this->rPrime();
+        case MOVE::R2:
+            return this->r2();
+        case MOVE::U:
+            return this->u();
+        case MOVE::UPRIME:
+            return this->uPrime();
+        case MOVE::U2:
+            return this->u2();
+        case MOVE::D:
+            return this->d();
+        case MOVE::DPRIME:
+            return this->dPrime();
+        case MOVE::D2:
+            return this->d2();
+        case MOVE::F:
+            return this->f();
+        case MOVE::FPRIME:
+            return this->fPrime();
+        case MOVE::F2:
+            return this->f2();
+        case MOVE::B:
+            return this->b();
+        case MOVE::BPRIME:
+            return this->bPrime();
+        case MOVE::B2:
+            return this->b2();
+    }
+}
+
+/*
+ * Invert a move.
+ */
+RubiksCube &RubiksCube::invert(MOVE ind) {
+    switch (ind) {
+        case MOVE::L:
+            return this->lPrime();
+        case MOVE::LPRIME:
+            return this->l();
+        case MOVE::L2:
+            return this->l2();
+        case MOVE::R:
+            return this->rPrime();
+        case MOVE::RPRIME:
+            return this->r();
+        case MOVE::R2:
+            return this->r2();
+        case MOVE::U:
+            return this->uPrime();
+        case MOVE::UPRIME:
+            return this->u();
+        case MOVE::U2:
+            return this->u2();
+        case MOVE::D:
+            return this->dPrime();
+        case MOVE::DPRIME:
+            return this->d();
+        case MOVE::D2:
+            return this->d2();
+        case MOVE::F:
+            return this->fPrime();
+        case MOVE::FPRIME:
+            return this->f();
+        case MOVE::F2:
+            return this->f2();
+        case MOVE::B:
+            return this->bPrime();
+        case MOVE::BPRIME:
+            return this->b();
+        case MOVE::B2:
+            return this->b2();
+    }
+}
+
+void RubiksCube::print() const {
+    cout << "Rubik Cube:\n\n";
+
+    for (int row = 2; row >= 0; row--) {
+        for (unsigned i = 0; i < 8; i++) cout << " ";
+        for (int col = 2; col >= 0; col--) {
+            switch (getColor(FACE::UP, row, col)) {
+                case COLOR::BLUE:
+                    cout << "B ";
+                    continue;
+                case COLOR::GREEN:
+                    cout << "G ";
+                    continue;
+                case COLOR::ORANGE:
+                    cout << "O ";
+                    continue;
+                case COLOR::RED:
+                    cout << "R ";
+                    continue;
+                case COLOR::WHITE:
+                    cout << "W ";
+                    continue;
+                case COLOR::YELLOW:
+                    cout << "Y ";
+                    continue;
+            }
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+    for (int row = 0; row <= 2; row++) {
+        for (int col = 0; col <= 2; col++) {
+            switch (getColor(FACE::LEFT, row, col)) {
+                case COLOR::BLUE:
+                    cout << "B ";
+                    continue;
+                case COLOR::GREEN:
+                    cout << "G ";
+                    continue;
+                case COLOR::ORANGE:
+                    cout << "O ";
+                    continue;
+                case COLOR::RED:
+                    cout << "R ";
+                    continue;
+                case COLOR::WHITE:
+                    cout << "W ";
+                    continue;
+                case COLOR::YELLOW:
+                    cout << "Y ";
+                    continue;
+            }
+        }
+        cout << " ";
+        for (int col = 0; col <= 2; col++) {
+            switch (getColor(FACE::FRONT, row, col)) {
+                case COLOR::BLUE:
+                    cout << "B ";
+                    continue;
+                case COLOR::GREEN:
+                    cout << "G ";
+                    continue;
+                case COLOR::ORANGE:
+                    cout << "O ";
+                    continue;
+                case COLOR::RED:
+                    cout << "R ";
+                    continue;
+                case COLOR::WHITE:
+                    cout << "W ";
+                    continue;
+                case COLOR::YELLOW:
+                    cout << "Y ";
+                    continue;
+            }
+        }
+        cout << " ";
+        for (int col = 0; col <= 2; col++) {
+            switch (getColor(FACE::RIGHT, row, col)) {
+                case COLOR::BLUE:
+                    cout << "B ";
+                    continue;
+                case COLOR::GREEN:
+                    cout << "G ";
+                    continue;
+                case COLOR::ORANGE:
+                    cout << "O ";
+                    continue;
+                case COLOR::RED:
+                    cout << "R ";
+                    continue;
+                case COLOR::WHITE:
+                    cout << "W ";
+                    continue;
+                case COLOR::YELLOW:
+                    cout << "Y ";
+                    continue;
+            }
+        }
+        cout << " ";
+        for (int col = 0; col <= 2; col++) {
+            switch (getColor(FACE::BACK, row, col)) {
+                case COLOR::BLUE:
+                    cout << "B ";
+                    continue;
+                case COLOR::GREEN:
+                    cout << "G ";
+                    continue;
+                case COLOR::ORANGE:
+                    cout << "O ";
+                    continue;
+                case COLOR::RED:
+                    cout << "R ";
+                    continue;
+                case COLOR::WHITE:
+                    cout << "W ";
+                    continue;
+                case COLOR::YELLOW:
+                    cout << "Y ";
+                    continue;
+            }
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+    for (int row = 0; row <= 2; row++) {
+        for (unsigned i = 0; i < 8; i++) cout << " ";
+        for (int col = 0; col <= 2; col++) {
+            switch (getColor(FACE::DOWN, row, col)) {
+                case COLOR::BLUE:
+                    cout << "B ";
+                    continue;
+                case COLOR::GREEN:
+                    cout << "G ";
+                    continue;
+                case COLOR::ORANGE:
+                    cout << "O ";
+                    continue;
+                case COLOR::RED:
+                    cout << "R ";
+                    continue;
+                case COLOR::WHITE:
+                    cout << "W ";
+                    continue;
+                case COLOR::YELLOW:
+                    cout << "Y ";
+                    continue;
+            }
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
+RubiksCube &RubiksCube::randomShuffleCube(unsigned int times) {
+    for (unsigned int i = 0; i < times; i++) {
+        unsigned int selectMove = (rand() % 18);
+        this->move(static_cast<MOVE>(selectMove));
+    }
+    return *this;
+}
---
+++ Model/RubiksCube.h
@@ -0,0 +1,146 @@
+//
+// Created by Shubham Patil on 17/12/21.
+//
+
+#ifndef RUBIKS_CUBE_SOLVER_RUBIKSCUBE_H
+#define RUBIKS_CUBE_SOLVER_RUBIKSCUBE_H
+
+#include "bits/stdc++.h"
+using namespace std;
+
+/**
+ * A base class for all Rubik's Cube Model. There are various representation for Rubik's Cube.
+ * Each one has it's own special ways of definitions. This class provides a shared functionality
+ * between all models.
+ * We'll benchmark all models and observe which one is better for performance.
+ */
+
+class RubiksCube {
+public:
+    enum class FACE {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        FRONT,
+        BACK
+    };
+
+    enum class COLOR {
+        WHITE,
+        GREEN,
+        RED,
+        BLUE,
+        ORANGE,
+        YELLOW
+    };
+
+    enum class MOVE {
+        L, LPRIME, L2,
+        R, RPRIME, R2,
+        U, UPRIME, U2,
+        D, DPRIME, D2,
+        F, FPRIME, F2,
+        B, BPRIME, B2
+    };
+
+    /*
+     * Returns the color of the cell at (row, col) in face.
+     * If Rubik's Cube face is pointing at your, then the row numbering starts from the
+     * top to bottom, and column numbering starts from the left to right.
+     * The rows and columns are 0-indexed.
+     * @param Face, row, and column index
+     */
+    virtual COLOR getColor(FACE face, unsigned row, unsigned col) const = 0;
+
+    /*
+     * Returns true if the Rubik Cube is solved, otherwise returns false.
+     */
+    virtual bool isSolved() const = 0;
+
+    /*
+     * Returns the move in the string format.
+     */
+    string getMove(MOVE ind) const;
+
+    /*
+     * Print the Rubik Cube in Planar format.
+     *
+     * The cube is laid out as follows.
+     *
+     * The sides:
+     *    U
+     *  L F R B
+     *    D
+     *
+     * Color wise:
+     *
+     *          W W W
+     *          W W W
+     *          W W W
+     *
+     *  G G G   R R R   B B B   O O O
+     *  G G G   R R R   B B B   O O O
+     *  G G G   R R R   B B B   O O O
+     *
+     *          Y Y Y
+     *          Y Y Y
+     *          Y Y Y
+     *
+     * Row and Column Numberings:
+     * rx -> row numbering
+     * cx -> column numbering
+     * bx -> both row and column numberint
+     *
+     *             .  .  r2
+     *             .  .  r1
+     *             c2 c1 b0
+     *
+     *  b0 c1 c2   b0 c1 c2   b0 c1 c2   b0 c1 c2
+     *  r1  .  .   r1  .  .   r1  .  .   r1  .  .
+     *  r2  .  .   r2  .  .   r2  .  .   r2  .  .
+     *
+     *             b0 c1 c2
+     *             r1  .  .
+     *             r2  .  .
+     *
+     */
+    void print() const;
+
+    /*
+     * Randomly shuffle the cube.
+     */
+    RubiksCube& randomShuffleCube(unsigned int times);
+
+    // Rotational moves on Rubik Cube
+
+    RubiksCube& move(MOVE ind);
+    RubiksCube& invert(MOVE ind);
+
+    virtual RubiksCube& u() = 0;
+    virtual RubiksCube& uPrime() = 0;
+    virtual RubiksCube& u2() = 0;
+
+    virtual RubiksCube& l() = 0;
+    virtual RubiksCube& lPrime() = 0;
+    virtual RubiksCube& l2() = 0;
+
+    virtual RubiksCube& f() = 0;
+    virtual RubiksCube& fPrime() = 0;
+    virtual RubiksCube& f2() = 0;
+
+    virtual RubiksCube& r() = 0;
+    virtual RubiksCube& rPrime() = 0;
+    virtual RubiksCube& r2() = 0;
+
+    virtual RubiksCube& b() = 0;
+    virtual RubiksCube& bPrime() = 0;
+    virtual RubiksCube& b2() = 0;
+
+    virtual RubiksCube& d() = 0;
+    virtual RubiksCube& dPrime() = 0;
+    virtual RubiksCube& d2() = 0;
+};
+
+
+#endif //RUBIKS_CUBE_SOLVER_RUBIKSCUBE_H
